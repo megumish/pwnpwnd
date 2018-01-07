05ccd02c62a008c5740950a78e94e2aa511889c6
Keishi Kawada <Kk-shli@outlook.jp>
2018-01-07 14:50:22 +0900

    environment variable test

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate dotenv;
+extern crate envy;
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize, Debug)]
+struct EnvOK {
+    ok: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use ::*;
+    use dotenv;
+    use envy;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 2 + 2);
+    }
+
+    #[test]
+    fn env_test() {
+        dotenv::dotenv().expect("Failed to read .env file");
+        match envy::from_env::<EnvOK>() {
+            Ok(env_ok) => assert_eq!(env_ok.ok, "OK"),
+            Err(err) => {
+                println!("Read Env Error: {}", err);
+                assert!(false);
+            }
+        }
+    }
+}
